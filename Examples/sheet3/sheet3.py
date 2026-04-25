b19714ebf7f254c47b03c271b5f82073653fbf0a
iyassienx86 <iyassien2005@.com>
2026-04-25 13:11:21 +0300

    Basic Logistic Regression

---
+++ Examples/sheet3/sheet3.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression(max_iter=10000)
+model.fit(X_train, y_train)
+
+y_prediction = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_prediction)
+
+print("--- Q1: Basic Logistic Regression ---")
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
